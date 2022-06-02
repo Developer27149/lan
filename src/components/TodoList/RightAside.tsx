ff2e378f5a0e8232d218afdd06fe317784d037cc
Developer27149 <5641151947@qq.com>
2022-06-03 02:11:58 +0800

    feat: :tada: todo list feature text

--- src/components/TodoList/RightAside.tsx
+++ src/components/TodoList/RightAside.tsx
@@ -2,7 +2,7 @@ export default function RightAside() {
   return (
     <div className="right-aside">
       <div className="right-aside-header">
-        <h3>没啥好说的，祝一切顺利</h3>
+        <h3>没啥好说的，祝一切顺利!</h3>
         <div>
           <input placeholder="啥事啊？" />
         </div>
